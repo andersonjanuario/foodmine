04eb0c64eb95fa93112123618dd3a6658c3e0268
januarioacsj <januarioacsj@fab.mil.br>
2023-07-18 16:57:00 -0300

    Add home + food service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // Rating - Implementar video correção -> https://www.youtube.com/watch?v=JIOrhLNAkcc
   ],
   providers: [],
   bootstrap: [AppComponent]
