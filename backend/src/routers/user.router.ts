4405b471f22b9298ec704bb9ae7114b7c7c84b44
Anderson Januário <anderson_januario@hotmail.com>
2023-07-22 23:12:28 -0300

    Register page + remoção id models atlas da erro

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -3,6 +3,8 @@ import { sample_users } from "../data";
 import jwt from 'jsonwebtoken';
 import asyncHandler from "express-async-handler";
 import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from 'bcryptjs';
 
 const router = Router();
 
@@ -25,21 +27,44 @@ router.post("/login", asyncHandler(
         if(user){
             res.send(generateTokenResponse(user));
         }else{
-            const BAD_REQUEST = 400;
-            res.status(BAD_REQUEST).send("User name or password is not valid!");
+            res.status(HTTP_BAD_REQUEST).send("User name or password is not valid!");
         }
     }
 ));
 
+router.post('/register', asyncHandler(
+  async (req, res) => {
+    const {name, email, password, address} = req.body;
+    const user = await UserModel.findOne({email});
+    if(user){
+      res.status(HTTP_BAD_REQUEST)
+        .send('User is already exist, please login!');
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser:User = {
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false
+    }
+
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser));
+  }
+));
+
 const generateTokenResponse = (user : User) => {
     const token = jwt.sign({
-      id: user.id, email:user.email, isAdmin: user.isAdmin
+      email:user.email, isAdmin: user.isAdmin
     },process.env.JWT_SECRET!,{
       expiresIn:"30d"
     });
   
     return {
-      id: user.id,
       email: user.email,
       name: user.name,
       address: user.address,
