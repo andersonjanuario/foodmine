01f8b854be1d66691bfdc47d6bff813cc315a376
Anderson Januário <anderson_januario@hotmail.com>
2023-07-25 12:01:57 -0300

    Ajuste validação de senha - descriptografar do banco

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -23,8 +23,9 @@ router.get("/seed", asyncHandler(
 router.post("/login", asyncHandler(
     async (req, res) => {
         const {email, password} = req.body;
-        const user = await UserModel.findOne({email, password});    
-        if(user){
+        const user = await UserModel.findOne({email});
+
+        if(user && (await bcrypt.compare(password,user.password))) {
             res.send(generateTokenResponse(user));
         }else{
             res.status(HTTP_BAD_REQUEST).send("User name or password is not valid!");
