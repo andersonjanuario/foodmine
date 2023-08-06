54cdd33c4f10fefece2594a8c32e75e798c6bfbf
Anderson Januário <anderson_januario@hotmail.com>
2023-08-05 23:51:22 -0300

    Alterando cors server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:["*"]
+    origin:["https://farmaquiz.onrender.com"]
 }));
 
 app.use("/api/foods", foodRouter);
