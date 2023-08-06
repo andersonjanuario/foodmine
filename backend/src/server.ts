e5169722a84a8a1cd4ec25db83e61669bbca996f
Anderson Januário <anderson_januario@hotmail.com>
2023-08-05 23:08:19 -0300

    Alterando porta

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 });
